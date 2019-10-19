ec53608cd486115bcc4d7de84c2e16940d045c9b
xiaoguo_ge@yahoo.com <xiaoguo_ge@yahoo.com>
2019-10-20 09:16:48 +1100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Character
+    {
+        public string Name { get; set; }
+    }
+    public class DTDroid
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    [GraphQLMetadata("Droid", IsTypeOf = typeof(Droid))]
+    public class DroidType
+    {
+        public string Id(DTDroid droid) => droid.Id;
+        public string Name(DTDroid droid) => droid.Name + "fake";
+
+        // these two parameters are optional
+        // ResolveFieldContext provides contextual information about the field
+        public Character Friend(ResolveFieldContext context, Droid source)
+        {
+            return new Character { Name = "C3-PO" };
+        }
+    }
+
+    public class Droid
+    {
+        //todo, how to define field resolvers
+        [GraphQLMetadata("name")]
+        public string droid(DTDroid droid)
+        {
+            return droid.Name + "fake";
+        }
+    }
+
+    public class Query
+    {
+        [GraphQLMetadata("droid")]
+        public DTDroid droid(string id)
+        {
+            return new[]
+            {
+                new DTDroid { Id = "123", Name = "R2-D2" },
+                new DTDroid { Id = "456", Name = "R3-D3" }
+            }
+            .FirstOrDefault(x => x.Id == id);
+        }
+
+        [GraphQLMetadata("droids")]
+        public IEnumerable<DTDroid> droids()
+        {
+            return new[]
+            {
+                new DTDroid { Id = "123", Name = "R2-D2" },
+                new DTDroid { Id = "456", Name = "R3-D3" }
+            };
+        }
+    }
+    class Program
+    {
+        const string _schema = @"type Droid {
+                                      id: ID!
+                                      name: String!
+                                    }
+
+                                    type Query {
+                                      droid(id: ID): Droid
+                                      droids: [Droid]
+                                    }";
+        static void Main(string[] args)
+        {
+            var schema = Schema.For(_schema, schemaBuilder =>
+            {
+                schemaBuilder.Types.Include<Query>();
+                schemaBuilder.Types.Include<DTDroid>();
+                schemaBuilder.Types.Include<DroidType>();
+            });
+
+            var json = schema.Execute(executionOptions =>
+            {
+                executionOptions.Query = $"{{ droid(id: \"123\") {{ id name }} }}";
+            });
+
+            json = schema.Execute(executionOptions =>
+            {
+                executionOptions.Query = $"{{ droids {{ id name }} }}";
+            });
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ GraphQLDemo/Controllers/GraphQLController.cs
@@ -0,0 +1,85 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace GraphQLDemo.Controllers
+{
+    public class Droid
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Query
+    {
+        private List<Droid> _droids = new List<Droid>
+                                          {
+                                            new Droid { Id = "123", Name = "R2-D2" },
+                                            new Droid { Id = "456", Name = "R2-D1" },
+                                            new Droid { Id = "789", Name = "R2-D3" }
+                                          };
+
+        [GraphQLMetadata("droid")]
+        public Droid GetDroid(string id)
+        {
+            return _droids.FirstOrDefault(x => x.Id == id);
+        }
+        [GraphQLMetadata("droids")]
+        public IEnumerable<Droid> GetDroids()
+        {
+            return _droids;
+        }
+    }
+    public class GraphQLController : ApiController
+    {
+        // GET: api/GraphQL
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/GraphQL/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/GraphQL
+        public string Post([FromBody]string query)
+        {
+            var schema = Schema.For(@"
+  type Droid {
+    id: ID
+    name: String
+  }
+
+  type Query {
+    droid(id: ID): Droid
+    droids: [Droid]
+  }
+", _ =>
+            {
+                _.Types.Include<Query>();
+            });
+
+            var json = schema.Execute(_ =>
+            {
+                _.Query = query;
+            });
+
+            return json;
+
+        }
+
+        // PUT: api/GraphQL/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/GraphQL/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
